b23e05364611b61ca4e6badeb8d2024d73190871
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2023-09-09 15:55:57 -0400

    Refactor the code using prettier

--- src/sections/home/gallery/gallery.tsx
+++ src/sections/home/gallery/gallery.tsx
@@ -3,36 +3,34 @@ import styles from "./gallery.module.scss";
 const { imageGalleryContainer, galleryTitle } = styles;
 
 export default function Gallery() {
-    return (
-        <section className="section mt-32">
-            <div className="container mx-auto">
-                <div className={`${imageGalleryContainer}`}>
-                    <h2>GALLERY</h2>
+  return (
+    <section className="section mt-32">
+      <div className="container mx-auto">
+        <div className={`${imageGalleryContainer}`}>
+          <h2>GALLERY</h2>
 
-                    <div className="grid grid-cols-3 h-full">
-                        <div className="col-span-2 flex flex-col justify-between">
-                            <div className={`flex gap-2`}>
-                                <div className="circle"></div>
-                                <div className="circle"></div>
-                                <div className="circle"></div>
-                            </div>
+          <div className="grid grid-cols-3 h-full">
+            <div className="col-span-2 flex flex-col justify-between">
+              <div className={`flex gap-2`}>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+              </div>
 
-                            <div className="grid grid-cols-3">
-                                <p className="col-span-2">
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua.
-                                    Elementum sagittis vitae et leo duis ut diam
-                                    quam.
-                                </p>
-                                <h1 className={`${galleryTitle} col-span-3 mt-4`}>
-                                    HACKCON x HTV 2023
-                                </h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <div className="grid grid-cols-3">
+                <p className="col-span-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Elementum sagittis vitae et leo duis ut diam quam.
+                </p>
+                <h1 className={`${galleryTitle} col-span-3 mt-4`}>
+                  HACKCON x HTV 2023
+                </h1>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/sections/skills/skills.tsx
+++ src/sections/skills/skills.tsx
@@ -2,161 +2,163 @@ import styles from "./skills.module.scss";
 import Image from "next/image";
 
 const { skillsContainer, skillContainer, skillNumber, scrollContainer } =
-    styles;
+  styles;
 
 export default function Skills() {
-    return (
-        <section className="section mt-32">
-            <div className="container mx-auto">
-                <h2><span className="font-extrabold">FEATURED</span> SKILLS</h2>
+  return (
+    <section className="section mt-32">
+      <div className="container mx-auto">
+        <h2>
+          <span className="font-extrabold">FEATURED</span> SKILLS
+        </h2>
 
-                <div className={`${skillsContainer} grid grid-cols-3 mt-6`}>
-                    <div className={`${skillContainer} flex justify-between`}>
-                        <div className="flex flex-col justify-between">
-                            <Image
-                                src={"/images/logo-react-icon.png"}
-                                width={40}
-                                height={40}
-                                alt="React"
-                            ></Image>
-                            <p className="descriptionText">REACT</p>
-                        </div>
-                        <div className="flex items-end justify-center">
-                            <div className={`${skillNumber}`}>
-                                <h1>1</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${skillContainer} flex justify-between`}>
-                        <div className="flex flex-col justify-between">
-                            <Image
-                                src={"/images/image 7.png"}
-                                width={40}
-                                height={40}
-                                alt="React"
-                            ></Image>
-                            <p className="descriptionText">VUE</p>
-                        </div>
-                        <div className="flex items-end justify-center">
-                            <div className={`${skillNumber}`}>
-                                <h1>2</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${skillContainer} flex justify-between`}>
-                        <div className="flex flex-col justify-between">
-                            <Image
-                                src={"/images/image 14.png"}
-                                width={40}
-                                height={40}
-                                alt="React"
-                            ></Image>
-                            <p className="descriptionText">FIGMA</p>
-                        </div>
-                        <div className="flex items-end justify-center">
-                            <div className={`${skillNumber}`}>
-                                <h1>3</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${skillContainer} flex justify-between`}>
-                        <div className="flex flex-col justify-between">
-                            <Image
-                                src={"/images/image 9.png"}
-                                width={40}
-                                height={40}
-                                alt="React"
-                            ></Image>
-                            <p className="descriptionText">EXPRESS</p>
-                        </div>
-                        <div className="flex items-end justify-center">
-                            <div className={`${skillNumber}`}>
-                                <h1>4</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${skillContainer} flex justify-between`}>
-                        <div className="flex flex-col justify-between">
-                            <Image
-                                src={"/images/image 14.png"}
-                                width={40}
-                                height={40}
-                                alt="React"
-                            ></Image>
-                            <p className="descriptionText">FIGMA</p>
-                        </div>
-                        <div className="flex items-end justify-center">
-                            <div className={`${skillNumber}`}>
-                                <h1>5</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${skillContainer} flex justify-between`}>
-                        <div className="flex flex-col justify-between">
-                            <Image
-                                src={"/images/logo-react-icon.png"}
-                                width={40}
-                                height={40}
-                                alt="React"
-                            ></Image>
-                            <p className="descriptionText">REACT</p>
-                        </div>
-                        <div className="flex items-end justify-center">
-                            <div className={`${skillNumber}`}>
-                                <h1>6</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${skillContainer} flex justify-between`}>
-                        <div className="flex flex-col justify-between">
-                            <Image
-                                src={"/images/logo-react-icon.png"}
-                                width={40}
-                                height={40}
-                                alt="React"
-                            ></Image>
-                            <p className="descriptionText">REACT</p>
-                        </div>
-                        <div className="flex items-end justify-center">
-                            <div className={`${skillNumber}`}>
-                                <h1>1</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${skillContainer} flex justify-between`}>
-                        <div className="flex flex-col justify-between">
-                            <Image
-                                src={"/images/image 7.png"}
-                                width={40}
-                                height={40}
-                                alt="React"
-                            ></Image>
-                            <p className="descriptionText">VUE</p>
-                        </div>
-                        <div className="flex items-end justify-center">
-                            <div className={`${skillNumber}`}>
-                                <h1>2</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${skillContainer} flex justify-between`}>
-                        <div className="flex flex-col justify-between">
-                            <Image
-                                src={"/images/image 14.png"}
-                                width={40}
-                                height={40}
-                                alt="React"
-                            ></Image>
-                            <p className="descriptionText">FIGMA</p>
-                        </div>
-                        <div className="flex items-end justify-center">
-                            <div className={`${skillNumber}`}>
-                                <h1>3</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className={`${skillsContainer} grid grid-cols-3 mt-6`}>
+          <div className={`${skillContainer} flex justify-between`}>
+            <div className="flex flex-col justify-between">
+              <Image
+                src={"/images/logo-react-icon.png"}
+                width={40}
+                height={40}
+                alt="React"
+              ></Image>
+              <p className="descriptionText">REACT</p>
             </div>
-        </section>
-    );
+            <div className="flex items-end justify-center">
+              <div className={`${skillNumber}`}>
+                <h1>1</h1>
+              </div>
+            </div>
+          </div>
+          <div className={`${skillContainer} flex justify-between`}>
+            <div className="flex flex-col justify-between">
+              <Image
+                src={"/images/image 7.png"}
+                width={40}
+                height={40}
+                alt="React"
+              ></Image>
+              <p className="descriptionText">VUE</p>
+            </div>
+            <div className="flex items-end justify-center">
+              <div className={`${skillNumber}`}>
+                <h1>2</h1>
+              </div>
+            </div>
+          </div>
+          <div className={`${skillContainer} flex justify-between`}>
+            <div className="flex flex-col justify-between">
+              <Image
+                src={"/images/image 14.png"}
+                width={40}
+                height={40}
+                alt="React"
+              ></Image>
+              <p className="descriptionText">FIGMA</p>
+            </div>
+            <div className="flex items-end justify-center">
+              <div className={`${skillNumber}`}>
+                <h1>3</h1>
+              </div>
+            </div>
+          </div>
+          <div className={`${skillContainer} flex justify-between`}>
+            <div className="flex flex-col justify-between">
+              <Image
+                src={"/images/image 9.png"}
+                width={40}
+                height={40}
+                alt="React"
+              ></Image>
+              <p className="descriptionText">EXPRESS</p>
+            </div>
+            <div className="flex items-end justify-center">
+              <div className={`${skillNumber}`}>
+                <h1>4</h1>
+              </div>
+            </div>
+          </div>
+          <div className={`${skillContainer} flex justify-between`}>
+            <div className="flex flex-col justify-between">
+              <Image
+                src={"/images/image 14.png"}
+                width={40}
+                height={40}
+                alt="React"
+              ></Image>
+              <p className="descriptionText">FIGMA</p>
+            </div>
+            <div className="flex items-end justify-center">
+              <div className={`${skillNumber}`}>
+                <h1>5</h1>
+              </div>
+            </div>
+          </div>
+          <div className={`${skillContainer} flex justify-between`}>
+            <div className="flex flex-col justify-between">
+              <Image
+                src={"/images/logo-react-icon.png"}
+                width={40}
+                height={40}
+                alt="React"
+              ></Image>
+              <p className="descriptionText">REACT</p>
+            </div>
+            <div className="flex items-end justify-center">
+              <div className={`${skillNumber}`}>
+                <h1>6</h1>
+              </div>
+            </div>
+          </div>
+          <div className={`${skillContainer} flex justify-between`}>
+            <div className="flex flex-col justify-between">
+              <Image
+                src={"/images/logo-react-icon.png"}
+                width={40}
+                height={40}
+                alt="React"
+              ></Image>
+              <p className="descriptionText">REACT</p>
+            </div>
+            <div className="flex items-end justify-center">
+              <div className={`${skillNumber}`}>
+                <h1>1</h1>
+              </div>
+            </div>
+          </div>
+          <div className={`${skillContainer} flex justify-between`}>
+            <div className="flex flex-col justify-between">
+              <Image
+                src={"/images/image 7.png"}
+                width={40}
+                height={40}
+                alt="React"
+              ></Image>
+              <p className="descriptionText">VUE</p>
+            </div>
+            <div className="flex items-end justify-center">
+              <div className={`${skillNumber}`}>
+                <h1>2</h1>
+              </div>
+            </div>
+          </div>
+          <div className={`${skillContainer} flex justify-between`}>
+            <div className="flex flex-col justify-between">
+              <Image
+                src={"/images/image 14.png"}
+                width={40}
+                height={40}
+                alt="React"
+              ></Image>
+              <p className="descriptionText">FIGMA</p>
+            </div>
+            <div className="flex items-end justify-center">
+              <div className={`${skillNumber}`}>
+                <h1>3</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
