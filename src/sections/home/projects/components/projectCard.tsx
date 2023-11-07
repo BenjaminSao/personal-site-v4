805ec5f4d343e06c3bdc3033ddb7b0312dfaad31
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2023-11-07 07:46:01 -0500

    Bug Fix

--- src/sections/home/projects/components/projectCard.tsx
+++ src/sections/home/projects/components/projectCard.tsx
@@ -5,43 +5,47 @@ import Image from "next/image";
 const { project, projectImage, projectImageOverlay } = styles;
 
 export interface ProjectCardProps {
-  id: number;
-  image?: string;
-  name: string;
-  tagline: string;
-  backgroundColor?: string;
-  onClick: () => void;
+    id: number;
+    image?: string;
+    name: string;
+    tagline: string;
+    backgroundColor?: string;
+    onClick: () => void;
+    key: number;
 }
 
 export default function ProjectCard(props: ProjectCardProps) {
-  return (
-    <div
-      className={`${project} cursor-pointer`}
-      data-aos="fade-up"
-      data-aos-delay={50 * props.id}
-      onClick={() => props.onClick()}
-    >
-      {props.image ? (
-        <Image
-          width={4320}
-          height={4320}
-          className={`${projectImage}`}
-          src={props.image}
-          alt=""
-        />
-      ) : (
+    return (
         <div
-          className={`${projectImage}`}
-          style={{ background: props.backgroundColor }}
-        ></div>
-      )}
+            className={`${project} cursor-pointer`}
+            data-aos="fade-up"
+            data-aos-delay={50 * props.id}
+            onClick={() => props.onClick()}
+            key={props.key}
+        >
+            {props.image ? (
+                <Image
+                    width={4320}
+                    height={4320}
+                    className={`${projectImage}`}
+                    src={props.image}
+                    alt=""
+                />
+            ) : (
+                <div
+                    className={`${projectImage}`}
+                    style={{ background: props.backgroundColor }}
+                ></div>
+            )}
 
-      <div className={`${projectImageOverlay}`}></div>
+            <div className={`${projectImageOverlay}`}></div>
 
-      <div className="h-[15%] px-8 pt-10">
-        <p className="descriptionText mb-2">{props.name.toUpperCase()}</p>
-        <p>{props.tagline}</p>
-      </div>
-    </div>
-  );
+            <div className="h-[15%] px-8 pt-10">
+                <p className="descriptionText mb-2">
+                    {props.name.toUpperCase()}
+                </p>
+                <p>{props.tagline}</p>
+            </div>
+        </div>
+    );
 }
